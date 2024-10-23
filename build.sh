d8ca0749d2a097888e65b670f7cacab480491991
adrianosergiodias@gmail.com <adrianosergiodias@gmail.com>
2024-10-22 22:53:36 -0400

    Atualizações no build.sh
    Atualize seu build.sh para criar o diretório /home/firebird antes de tentar criar o arquivo

--- build.sh
+++ build.sh
@@ -1,9 +1,13 @@
+
 #!/bin/bash
 
 # Script para baixar e instalar o Firebird
 wget $FBURL -O /tmp/firebird.tar.xz
 mkdir -p $PREFIX
 tar -xf /tmp/firebird.tar.xz -C $PREFIX --strip-components=1
+
+# Criar o diretório para salvar firebird.tar.gz
+mkdir -p /home/firebird
 
 # Compressão do diretório de instalação em firebird.tar.gz
 tar -czf /home/firebird/firebird.tar.gz -C $PREFIX .
